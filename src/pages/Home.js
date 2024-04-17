25b3ec87ff65965888c356e8a755527aa4643caf
Yorick Devleeschouwer <yorick.devleeschouwer@gmail.com>
2024-04-17 18:09:21 +0200

    Fix: all achievement sections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,28 @@ function Home(props) {
   
     return (
         <div>
-            <div className='viewheight-centered-block'>
+            <div className='section section-no-pad'>
                 <a href="#section-1" className='fp-title'>{props.title}</a>
             </div>
-            <div className='section' id='section-1'>
+            <div className='section section-no-pad' id='section-1'>
                 <a href="#section-code" className='mini-link-title'>I Code</a>
                 <a href="#section-dj" className='mini-link-title'>I DJ</a>
                 <a href="#section-design" className='mini-link-title'>I Design</a>
             </div>
-            <div className='section' id='section-code'>
-                <Achievement title="Beach Puppies (game) | JavaScript - XD year 1" link="https://yorickdv.be/projects/1IMD/BeachPuppies/game.html" type="game" place="1 XD" language="JavaScript" />
-                <Achievement title="Cub Defender (game) | JavaScript - XD year 1" link="https://yorickdv.be/projects/1IMD/clubdefender/portrait.html" type="game" place="1 XD" language="JavaScript" />
-                <Achievement title="Copoll | php - XD year 2" link="https://github.com/yoyoplayer12/desity" type="Community platform" place="2 XD" language="php" />
+            <div className='section section-blocks' id='section-code'>
+                <Achievement title="Beach Puppies" link="https://yorickdv.be/projects/1IMD/BeachPuppies/game.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Cub Defender" link="https://yorickdv.be/projects/1IMD/clubdefender/portrait.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Copoll" link="https://github.com/yoyoplayer12/desity" type="Community platform" place="2 XD" language="php" />
+            </div>
+            <div className='section section-blocks' id='section-dj'>
+                <Achievement title="Beach Puppies" link="https://yorickdv.be/projects/1IMD/BeachPuppies/game.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Cub Defender" link="https://yorickdv.be/projects/1IMD/clubdefender/portrait.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Copoll" link="https://github.com/yoyoplayer12/desity" type="Community platform" place="2 XD" language="php" />
+            </div>
+            <div className='section section-blocks' id='section-design'>
+                <Achievement title="Beach Puppies" link="https://yorickdv.be/projects/1IMD/BeachPuppies/game.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Cub Defender" link="https://yorickdv.be/projects/1IMD/clubdefender/portrait.html" type="Game" place="1 XD" language="JavaScript" />
+                <Achievement title="Copoll" link="https://github.com/yoyoplayer12/desity" type="Community platform" place="2 XD" language="php" />
             </div>
         </div>
     );
--- src/pages/components/Achievement.js
+++ src/pages/components/Achievement.js
@@ -4,7 +4,7 @@ function Achievement(props) {
     return (
       <a href={props.link} className='achievement' target='_blank' rel='noreferrer'>
         <h2>{props.title}</h2>
-        <p>{props.type} - {props.place}</p>
+        <p className='grey-text'>{props.type} - {props.place}</p>
         <p className='pill'>{props.language}</p>
       </a>
     );
